e08a105eb3c8f69c99eefbcf5bd04b95b1464f7d
Hassan-Laraik <hassanlaraik@gmail.com>
2023-12-28 11:40:29 +0100

    Ajoout tp 10 recherche

---
+++ tp10/backup/unit1.pas
@@ -0,0 +1,50 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, DB, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  DBGrids, DBCtrls, ZDataset, ZConnection;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BrtRechercher: TButton;
+    DsStagiaires: TDataSource;
+    DBGrid1: TDBGrid;
+    DBNavigator1: TDBNavigator;
+    EdtRechercher: TEdit;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    Znx: TZConnection;
+    ZtblStagiaires: TZTable;
+    procedure BrtRechercherClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.BrtRechercherClick(Sender: TObject);
+begin
+  ZtblStagiaires.Filtered := False;
+  ZtblStagiaires.Filter := 'concat(code,noms) like '+  quotedstr('*'+edtRechercher.text + '*');
+  ZtblStagiaires.Filtered := true;
+end;
+
+end.
+
